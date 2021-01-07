7a93ff6427a8ea7341843ac1aad09e8cd49b2203
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-01-07 18:16:34 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 module.exports = {
+    images: {
+      imageSizes: [18, 36, 54, 24, 48, 72, 96, 144],
+      domains: [
+        'dl.airtable.com',
+        'emoji.slack-edge.com',
+        'cloud-lp0r5yk68.vercel.app'
+      ]
+    },
     async rewrites() {
       return [
         {
           source: '/',
-          destination: `https://scrapbook.hackclub.com/rishi/`,
+          destination: `https://scrapbook.hackclub.com/${process.env.username}/`,
         }
       ]
     },
